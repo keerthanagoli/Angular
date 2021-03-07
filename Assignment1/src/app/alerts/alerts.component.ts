128601bc1e3c6ea2333332371fe5be494f8914bc
saikeerthana.goli <saikeerthana.goli@servicenow.com>
2021-03-08 00:27:22 +0530

    Udemy: Assignment 1 Components

---
+++ Assignment1/src/app/alerts/alerts.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-alert',
+    templateUrl:'./alerts.component.html',
+    styleUrls:['./alerts.component.css']
+})
+export class AlertComponent {}
